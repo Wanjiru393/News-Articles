6f5830ac082713dc3bba26ed8d56de7e2a5aa8bc
Wanjiru393 <carolciru393@gmail.com>
2022-05-03 20:43:09 +0300

    Testing Template Inheritance

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+# from newsapi import NewsApiClient
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+# @app.route("/")
+# def index():
+#     newsapi = NewsApiClient(api_key="3ca69fda5fa74125b35d01d178d09d42")
+
 
 
 if __name__ == "__main__":
