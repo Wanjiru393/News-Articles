c44750409cbbc416fdfee2e3bb7c1b18f9d778bf
Wanjiru393 <carolciru393@gmail.com>
2022-05-03 21:29:15 +0300

    Added behavior tests

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("index.html", content="Testing")
 
 
 # @app.route("/")
@@ -15,4 +15,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
