5e605c7ada35d392cbf639451f2871d104734b79
Wanjiru393 <carolciru393@gmail.com>
2022-05-04 15:51:55 +0300

    Deploying

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,request,redirect,url_for
-from main import *
+from .import main
 from ..request import get_sources, get_top_articles,get_keyword, get_breaking_news
 
 # Views
